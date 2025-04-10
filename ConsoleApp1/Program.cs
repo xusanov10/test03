3f4e75c835be0a8092f28359cf2cffa953300003
xusanov10 <ahmadjonxusanovv>
2025-04-10 19:14:01 +0500

    change1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void Main(string[] args)
+{
+    int[] ints = new int[10];
+    for (int i = 0; i < 10; i++)
+    {
+        ints[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    int counter = 0;
+    for (int i = 0; i < ints.Length; i++)
+    {
+        counter += ints[i];
+    }
+    Console.WriteLine(counter / 10);
+}
